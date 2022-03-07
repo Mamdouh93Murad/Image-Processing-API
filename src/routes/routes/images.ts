827315ddaf3b372d4196c31f01e0e3bd66eeb375
Mamdouh93Murad <mamdouh93morad@gmail.com>
2022-03-08 01:47:58 +0200

    Image EndPoint if statement
    added IF statement for when user doesn't specify filename for endpoint images, to show requirements and how to proceed

--- src/routes/routes/images.ts
+++ src/routes/routes/images.ts
@@ -12,35 +12,38 @@ images.get(
     const name : string = req.query.name as string
     let width : number = req.query.width as unknown as number
     let height : number = req.query.height as unknown as number
-    if (width === undefined) {
-      if (names.includes(name)) {
-        if (promises.existsSync(process.cwd() + '/images/full/' + name + '.jpg')) {
-          const file = process.cwd() + '/images/full/' + name + '.jpg'
-          res.sendFile(file)
+    if (name !== undefined) {
+      if (width === undefined) {
+        if (names.includes(name)) {
+          if (promises.existsSync(process.cwd() + '/images/full/' + name + '.jpg')) {
+            const file = process.cwd() + '/images/full/' + name + '.jpg'
+            res.sendFile(file)
+          }
+        }
+      } else {
+        if (names.includes(name)) {
+        // eslint-disable-next-line node/no-deprecated-api
+          if (promises.existsSync(process.cwd() + '/images/full/' + name + '.jpg')) {
+            const folder : string = width as unknown as string
+            if (!promises.existsSync(process.cwd() + '/images/full/' + folder)) {
+              await promises.mkdir(process.cwd() + '/images/full/' + folder, () => { console.log('CREATED') })
+            }
+            if (!promises.existsSync(process.cwd() + '/images/full/' + folder + '/' + name + '.jpg')) {
+              width = await Number(width)
+              height = await Number(height)
+              const image = await convert(name + '.jpg', width, height)
+              await save(image, name, folder)
+            }
+            const file = process.cwd() + '/images/full/' + folder + '/' + name + '.jpg'
+            res.sendFile(file)
+          }
+        } else {
+          res.send('FILE DOES NOT EXIST, WRONG IMAGE NAME')
         }
       }
     } else {
-      if (names.includes(name)) {
-        // eslint-disable-next-line node/no-deprecated-api
-        if (promises.existsSync(process.cwd() + '/images/full/' + name + '.jpg')) {
-          const folder : string = width as unknown as string
-          if (!promises.existsSync(process.cwd() + '/images/full/' + folder)) {
-            await promises.mkdir(process.cwd() + '/images/full/' + folder, () => { console.log('CREATED') })
-          }
-          if (!promises.existsSync(process.cwd() + '/images/full/' + folder + '/' + name + '.jpg')) {
-            width = await Number(width)
-            height = await Number(height)
-            const image = await convert(name + '.jpg', width, height)
-            await save(image, name, folder)
-          }
-          const file = process.cwd() + '/images/full/' + folder + '/' + name + '.jpg'
-          res.sendFile(file)
-        }
-      } else {
-        res.send('FILE DOES NOT EXIST, WRONG IMAGE NAME')
-      }
+      res.send('Please Specify Image name and Optionally Width and Height Value')
     }
-  }
-)
+  })
 
 export default images
